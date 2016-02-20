418711d03711c00fe924f716741342dd98c713bb
SGJTeam4 <sgj.team4.space.colony@gmail.com>
2016-02-20 23:04:05 +0900

    [プレイ画面]コンパイルエラーを修正

--- SpaceColony/Assets/Scripts/PlaySceneGameManager.cs
+++ SpaceColony/Assets/Scripts/PlaySceneGameManager.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 //using System;
 
 public class PlaySceneGameManager : MonoBehaviour {
+	//オブジェクト参照用変数
 	public GameObject shipModel;
 	public Text leftUpText; 
 	public Text Log1;
@@ -13,20 +14,14 @@ public class PlaySceneGameManager : MonoBehaviour {
 	public Text ShigenPercent;
 	public Text KankyoPercent;
 
-
-	private int time = 0;
+	private int time = 0;//とりあえずのTime用変数
 	public int nowYear = 0;
-
-	public string log1 = "test1";
-	public string log2 = "text2";
-	public string log3 = "テスト３";
-	public string leftUpStr;
-	public string eventLog= "イベント１";
+	public string leftUpStr;//左上のUIの文字用
 
 	//基本パラメーター
-	public double jinkoValue;
-	public double shigenValue;
-	public double kankyoValue;
+	public int jinkoValue;
+	public int shigenValue;
+	public int kankyoValue;
 
 	//強化項目の変数
 	private int shigenSaiseiLevel = 1;
@@ -37,7 +32,6 @@ public class PlaySceneGameManager : MonoBehaviour {
 	private int eiseiLevel = 1;
 	private int shigenSaisyuLevel = 1;
 
-
 	//イベントに関する変数
 	private int insekiDamage = 30;
 	private int infuruDamage = 30;
@@ -46,18 +40,21 @@ public class PlaySceneGameManager : MonoBehaviour {
 	private int mizuOsenDamage = 40;
 
 
-	void Awake(){
 
+	//---------------------------------------------------------
+	//初期化用の関数群
+	//---------------------------------------------------------
+	void Awake(){
 		Log1.text = "";
 		Log2.text = "";
 		Log3.text = "";
 	}
 
-	// Use this for initializationys
 	void Start () {
 		int tmp = PlayerPrefs.GetInt (PlayerPrefsKeys.FirstFlag,0);
+		tmp = 0;//とりあえず毎回初回起動フラグを立たせるためのゴリ押し。本来はタイトル画面で適切な処理をすると必要なくなります
 
-		Debug.Log("AAA" + (tmp));
+		//Debug.Log("AAA" + (tmp));
 		if (tmp == 0) {//初回起動のとき
 			PlayerPrefs.SetInt (PlayerPrefsKeys.FirstFlag, 1);
 			jinkoValue = 80;
@@ -69,6 +66,9 @@ public class PlaySceneGameManager : MonoBehaviour {
 			kankyoValue = PlayerPrefs.GetInt (PlayerPrefsKeys.Environment);
 		}
 	}
+
+
+	//情報表示ログを追加する
 	void addLog(string str){
 		Log3.text = Log2.text;
 		Log2.text = Log1.text;
@@ -82,7 +82,6 @@ public class PlaySceneGameManager : MonoBehaviour {
 	//-------------------------------------------------------
 
 	void jinkoUpdate(){
-
 		jinkoValue += 2;
 		if (jinkoValue > 100) {
 			jinkoValue = 100;
@@ -95,9 +94,6 @@ public class PlaySceneGameManager : MonoBehaviour {
 
 	void kankyoUpdate(){
 		kankyoValue += kankyoSyuhukuLevel;
-		if (kankyoValue > 100) {
-			kankyoValue = 100;
-		}
 	}
 
 
@@ -175,32 +171,49 @@ public class PlaySceneGameManager : MonoBehaviour {
 		leftUpStr = "超スーパー銀河暦" + nowYear + "年";
 		leftUpText.text = leftUpStr;
 
+		//ターン毎の状態更新と修正
 		jinkoUpdate ();
 		shigenUpdate ();
 		kankyoUpdate ();
+		paraSyusei ();
+
+		//イベント時の状態更新と修正
 		eventUpdate ();
+		paraSyusei ();
 
-
+		//UIに出力
 		JinkoPercent.text = jinkoValue + "%";
 		ShigenPercent.text = shigenValue + "%";
 		KankyoPercent.text = kankyoValue + "%";
-
 	}
 
-	// Update is called once per frame
+	void paraSyusei(){
+		if (jinkoValue > 100)
+			jinkoValue = 100;
+		if (jinkoValue < 0)
+			jinkoValue = 0;
+		if (kankyoValue > 100)
+			kankyoValue = 100;
+		if (kankyoValue < 0)
+			kankyoValue = 0;
+		if (shigenValue > 100)
+			shigenValue = 100;
+		if (shigenValue < 0)
+			shigenValue = 0;
+	}
+
 	void Update () {
 		if (time % 60 == 0) {
 			yearUpdate ();
 		}
 		time++;
 
+
 		shipModel.transform.Rotate (
 			new Vector3(
-				0,0,
-				//0,//Input.GetAxis("Vertical") * 60.0f * Time.deltaTime,
+				0,
+				0,
 				-0.2f * 60.0f * Time.deltaTime
-
-			
 			),
 			Space.Self
 		);
@@ -216,16 +229,11 @@ public class PlaySceneGameManager : MonoBehaviour {
 	}
 
 
-	void refresh(){
 
-		JinkoPercent.text = jinkoValue + "%";
-		ShigenPercent.text = shigenValue + "%";
-		KankyoPercent.text = kankyoValue + "%";
-	}
 
-	/// <summary>
-	/// ボタン系関数
-	/// </summary>
+	//---------------------------------------------------------
+	//ボタンクリック時の関数
+	//---------------------------------------------------------
 	public void kankyoClicked(){
 		saveData ();
 		/*
