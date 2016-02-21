90f79fab5ce8538f49376aa2b5ad8d830ea0523d
kulosuzu <kulosuzu.0109@gmail.com>
2016-02-21 10:57:26 +0900

    差分をプッシュ

---
+++ SpaceColony/Assets/Scripts/PlayerPrefsKeys.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPrefsKeys {
+	public static readonly string Resource = "Resource";//資源
+	public static readonly string Population = "Population";//人口
+	public static readonly string Environment = "Environment";//環境
+	public static readonly string CollectResource = "CollectResource";//資源採取
+	public static readonly string RestoreEnvironment= "RestoreEnvironment";//環境修復
+	public static readonly string Equipment= "Equipment";//装備
+	public static readonly string Medicine= "Medicine";//医学
+	public static readonly string Food= "Food";//食料
+	public static readonly string ReproduceResource= "ReproduceResource";//資源再生
+	public static readonly string Health= "Health";//衛生
+	public static readonly string FirstFlag = "FirstFlag";//初回起動フラグ  タイトルで０に初期化設定してほしい
+}
